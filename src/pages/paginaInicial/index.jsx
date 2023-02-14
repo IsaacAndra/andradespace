e572fca9e8498ee6f7a3e0f97b98c2a2af975c0e
IsaacAndra <isaac.andra84@gmail.com>
2023-02-14 17:14:27 -0300

    addicting Banner

--- src/pages/paginaInicial/index.jsx
+++ src/pages/paginaInicial/index.jsx
@@ -1,11 +1,21 @@
 import Cabecalho from "../../components/Cabecalho";
 import Menu from "../../components/Menu";
+import Banner from './banner.png';
+import styles from './paginaInicial.module.scss';
 
 export default function PaginaInicial(){
     return (
         <>
             <Cabecalho/>
-            <Menu/>
+            <main>
+                <section className={styles.principal}>
+                    <Menu/>
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço.</h1>
+                        <img src={Banner} alt="A imagem da terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
